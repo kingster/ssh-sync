308ce7a79411f8faf4dc802aefed532261e6fbd3
Kinshuk Bairagi <me@kinshuk.in>
2017-04-22 00:40:05 +0530

    Added down sync

--- sync.sh
+++ sync.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 set -e
 
-for file in `ls ~/.ssh/ -I authorized_keys -I known_hosts -I *.bak`
-do
-  name=`basename $file`
-  dest="Secure Notes\\SSH/$name"
-  echo "Syncing $file to $dest"
-  cat ~/.ssh/$file  | lpass edit --non-interactive  --notes "$dest"
+FILES=`ls ~/.ssh/ -I authorized_keys -I known_hosts`
+for file in $FILES ; do
+	name=`basename $file`
+	dest="Secure Notes\\SSH/$name"
+	echo "Syncing $file to $dest"
+	cat ~/.ssh/$file  | lpass edit --non-interactive  --notes "$dest"
 done
 
 echo "Syncing..."
 lpass sync
 
-lpass ls "Secure Notes\\SSH"
+UPSTREAM=`lpass ls "Secure Notes\\SSH" --color=never | tr -d ' '`
+for entry in $UPSTREAM; do
+	file=`echo $entry | cut -d '/' -f 2 | cut -d '[' -f 1`
+	 if [ ! -f ~/.ssh/$file ] && [ ! -d ~/.ssh/$file ]; then
+	    echo "Creating ~/.ssh/$file"
+	    id=`echo $entry | cut -d ":" -f 2 | tr -d '[]'`
+	    lpass show "$id" --notes >  ~/.ssh/$file
+	fi	
+done	
 
-echo "Failures....."
+# lpass ls "Secure Notes\\SSH"
 
-lpass ls "Secure Notes\\SSH" --color=never | grep -w "\b[id: 0]\b"
+# echo "Failures....."
+# lpass ls "Secure Notes\\SSH" --color=never | grep -w "\b[id: 0]\b"
 
